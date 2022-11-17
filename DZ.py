6c3b046754b5e1eb0d7b4e024e6c1830833d7e5d
TheBestOfTheBests <118480774+TheBestOfTheBests@users.noreply.github.com>
2022-11-17 23:35:41 +0300

    Update DZ.py

--- DZ.py
+++ DZ.py
@@ -3,6 +3,8 @@
 # c=input("Введите число c ")
 # print(c,b,a)
 
+
+
 # name="Иван"
 # age=15
 # nameyou=input("Как вас зовут? ")
@@ -11,3 +13,20 @@
 # print("Мне " +str(age) +" лет!")
 # print("А вам, как я уже понял, " +ageyou +", круто, больше не чего сказать.")
 # print("Вы хороший собеседник, но мне пора, удачи!")
+
+
+
+# import math
+# k1=input("Количество учеников в 1-ом кабинете: ")
+# print()
+# k2=input("Количество учеников в 2-ом кабинете: ")
+# print()
+# k3=input("Количество учеников в 3-ом кабинете: ")
+# k11=float(int(k1)/2)
+# p1=math.ceil(k11)
+# k22=float(int(k2)/2)
+# p2=math.ceil(k22)
+# k33=float(int(k3)/2)
+# p3=math.ceil(k33)
+# vseparti=int(p1+p2+p2)
+# print("Всего нужно купить " +str(vseparti) +" парт")
